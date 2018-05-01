3dc1a3b3d234e204268e9fb6487f5ab2816c2186
Jake Mitchell <jake@jakemitchell.xyz>
2018-05-01 15:51:12 -0400

    added history middleware to fix request routing

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const sqreen = require('sqreen') // App Security Monitoring Plugin
 const newrelic = require('newrelic') // App Performance Monitoring Plugin
 
 const express = require('express')
-const helmet = require('helmet')
-const compression = require('compression')
+const helmet = require('helmet') // common security setting fixes
+const compression = require('compression') // gz compression
+const history = require('connect-history-api-fallback') // reroute all requests to index.html, for VueJS routing
+
 const app = express()
 
+// Register middleware
 app.use(compression())
 app.use(helmet())
+app.use(history())
 
 // Listen to the dist folder
 app.use(express.static('dist'))
 
+// Turn on server
 app.listen(process.env.PORT, () => console.log('jakemitchell.xyz listening on port ' + process.env.PORT + '!'))
